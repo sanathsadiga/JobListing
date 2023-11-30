b790a863d99c7f621a2b72ce181627261efc6c76
sanathsadiga <114714181+sanathsadiga@users.noreply.github.com>
2023-11-30 19:35:51 +0530

    Add files via upload

---
+++ src/main/java/com/telusko/joblisting/repository/PostRepository.java
@@ -0,0 +1,8 @@
+package com.telusko.joblisting.repository;
+import com.telusko.joblisting.model.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PostRepository extends MongoRepository<Post,String>
+{
+
+}
